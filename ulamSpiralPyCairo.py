90b57017411f7067351c77ea36ba38d284f63f30
Daniel K.Gunleiksrud <591291@stud.hvl.no>
2022-03-17 08:13:14 +0100

    Sieve of Eratosthenes
    Sieve of Eratosthenes is faster than my homemade method for finding primes

--- ulamSpiralPyCairo.py
+++ ulamSpiralPyCairo.py
@@ -1,16 +1,26 @@
 import math
 import cairo
 
-WIDTH, HEIGHT = 3840, 2160
+WIDTH, HEIGHT = 3000, 3000
 
-surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, WIDTH)
+surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
 ctx = cairo.Context(surface)
 
 ctx.set_source_rgba(0.0, 0.0, 0.0, 1) 
-ctx.rectangle(0, 0, WIDTH, WIDTH)
+ctx.rectangle(0, 0, WIDTH, HEIGHT)
 ctx.fill()
-ctx.set_source_rgba(0.0, 1.0, 1.0, 1) 
+ctx.set_source_rgba(0.0, 1.0, 0.0, 1) 
 
+def primes_sieve(limit):
+    limitn = limit+1
+    primes = dict()
+    for i in range(2, limitn): primes[i] = True
+
+    for i in primes:
+        factors = range(i,limitn, i)
+        for f in factors[1:]:
+            primes[f] = False
+    return [i for i in primes if primes[i]==True]
 
 def findPrime(n):
     numberOfDividable = 0
@@ -50,8 +60,9 @@ j = 0
 x = WIDTH//2
 y = HEIGHT//2
 
-primeArray = primeList(WIDTH*WIDTH)
+primeArray = primes_sieve(WIDTH*WIDTH)
 
+print("Primes found")
 
 while(j < len(primeArray)):
 
@@ -91,4 +102,4 @@ while(j < len(primeArray)):
 
 
 surface.write_to_png("example.png")  # Output to PNG
-print("done")
+print("Drawing done")
