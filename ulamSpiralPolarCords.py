d690e4551ca30ff27467315ee7e1aacc6880141b
Daniel K.Gunleiksrud <591291@stud.hvl.no>
2022-03-20 14:19:11 +0100

    cleaned up unused variables + some comments

--- ulamSpiralPolarCords.py
+++ ulamSpiralPolarCords.py
@@ -1,6 +1,7 @@
 import math
 import cairo
 
+#Changeable variables
 WIDTH, HEIGHT = 1000, 1000
 SIZE = 1
 NUMBER_OF_PRIMES = 2_00_000
@@ -8,11 +9,13 @@ NUMBER_OF_PRIMES = 2_00_000
 surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
 ctx = cairo.Context(surface)
 
+#Black background
 ctx.set_source_rgba(0.0, 0.0, 0.0, 1) 
 ctx.rectangle(0, 0, WIDTH, HEIGHT)
 ctx.fill()
 ctx.set_source_rgba(0.0, 1.0, 0.0, 1) 
 
+#sieve of eratosthenes
 def primes_sieve(limit):
     limitn = limit+1
     primes = dict()
@@ -31,6 +34,7 @@ def nonPrime_color():
     ctx.set_source_rgba(1.0, 1.0, 0.0, 1) 
 
 def draw_dot(x):
+    #radius scaled to how many primes
     radius = (x/primeArray[-1])*WIDTH
     xpos = WIDTH//2 + radius * math.cos(x)
     ypos = HEIGHT//2 - radius * math.sin(x)
@@ -38,16 +42,14 @@ def draw_dot(x):
     #ctx.rectangle(xpos, ypos,SIZE,SIZE)
     ctx.fill()
 
-i = 1
-j = 0
-
-x = WIDTH//2
-y = HEIGHT//2
+i = 1 #Natural number
+j = 0 #Prime counter
 
 primeArray = primes_sieve(NUMBER_OF_PRIMES)
 
 print("Primes found")
 
+#loop through all primes found
 while(j < len(primeArray)):
 
     if primeArray[j] == i:
@@ -56,9 +58,8 @@ while(j < len(primeArray)):
         j += 1
     #else:
     #   nonPrime_color()
-        draw_dot(i)
-    
-    
+    #    draw_dot(i)
+
     i+= 1
 
 surface.write_to_png("example.png")  # Output to PNG
