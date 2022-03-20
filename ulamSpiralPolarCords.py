63cee6aabdfbf55d0ae835ee8c7052ca95ab9aa0
Daniel K.Gunleiksrud <591291@stud.hvl.no>
2022-03-20 16:33:47 +0100

    faster sieve of eratosthenes:))

--- ulamSpiralPolarCords.py
+++ ulamSpiralPolarCords.py
@@ -1,10 +1,11 @@
 import math
 import cairo
+import numpy
 
 #Changeable variables
 WIDTH, HEIGHT = 1000, 1000
 SIZE = 1
-NUMBER_OF_PRIMES = 2_00_000
+NUMBER_OF_PRIMES = 50_000_000
 
 surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
 ctx = cairo.Context(surface)
@@ -27,6 +28,29 @@ def primes_sieve(limit):
             primes[f] = False
     return [i for i in primes if primes[i]==True]
 
+def primes_sieve_v2(limit):
+    limitn = limit + 1
+    primes = dict()
+    for i in range(2, limitn):
+        primes[i] = True
+
+    pf = 2
+    while pf ** 2 <= limit:
+        if primes[pf]:
+            for i in range(pf ** 2, limitn, pf):
+                primes[i] = False
+        pf += 1
+
+    return primes
+
+def primes_sieve_v3(n):
+    flags = numpy.ones(n, dtype=bool)
+    flags[0] = flags[1] = False
+    for i in range(2, n):
+        if flags[i]:
+            flags[i*i::i] = False
+    return numpy.flatnonzero(flags)
+
 def prime_color():
     ctx.set_source_rgba(0.0, 0.0, 1.0, 1) 
 
@@ -35,7 +59,7 @@ def nonPrime_color():
 
 def draw_dot(x):
     #radius scaled to how many primes
-    radius = (x/primeArray[-1])*WIDTH
+    radius = (x/maxRad)*WIDTH
     xpos = WIDTH//2 + radius * math.cos(x)
     ypos = HEIGHT//2 - radius * math.sin(x)
     ctx.arc(xpos,ypos,SIZE,0,2*math.pi)
@@ -45,15 +69,18 @@ def draw_dot(x):
 i = 1 #Natural number
 j = 0 #Prime counter
 
-primeArray = primes_sieve(NUMBER_OF_PRIMES)
+primeArray = primes_sieve_v3(NUMBER_OF_PRIMES)
+
+maxRad = primeArray[-1]
 
 print("Primes found")
+prime_color()
 
 #loop through all primes found
 while(j < len(primeArray)):
 
     if primeArray[j] == i:
-        prime_color()
+        #prime_color()
         draw_dot(i)
         j += 1
     #else:
