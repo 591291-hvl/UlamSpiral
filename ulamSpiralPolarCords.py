2cbe93925551af4d55f8fc1fcaba9ed2c668874a
Daniel K.Gunleiksrud <591291@stud.hvl.no>
2022-03-20 14:13:07 +0100

    more compact

--- ulamSpiralPolarCords.py
+++ ulamSpiralPolarCords.py
@@ -1,9 +1,9 @@
 import math
 import cairo
 
-WIDTH, HEIGHT = 2000, 2000
-SIZE = 2
-NUMBER_OF_PRIMES = 1_00_000
+WIDTH, HEIGHT = 1000, 1000
+SIZE = 1
+NUMBER_OF_PRIMES = 2_00_000
 
 surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
 ctx = cairo.Context(surface)
@@ -24,24 +24,19 @@ def primes_sieve(limit):
             primes[f] = False
     return [i for i in primes if primes[i]==True]
 
-def draw_prime(x):
+def prime_color():
     ctx.set_source_rgba(0.0, 0.0, 1.0, 1) 
-    radius = (x/primeArray[-1])*WIDTH
-    xpos = WIDTH//2 + radius * math.cos(x)
-    ypos = HEIGHT//2 - radius * math.sin(x)
-    ctx.arc(xpos,ypos,SIZE,0,2*math.pi)
-    #ctx.rectangle(xpos, ypos,SIZE,SIZE)
-    ctx.fill()
 
-def draw_nonPrime(x):
+def nonPrime_color():
     ctx.set_source_rgba(1.0, 1.0, 0.0, 1) 
+
+def draw_dot(x):
     radius = (x/primeArray[-1])*WIDTH
     xpos = WIDTH//2 + radius * math.cos(x)
     ypos = HEIGHT//2 - radius * math.sin(x)
     ctx.arc(xpos,ypos,SIZE,0,2*math.pi)
     #ctx.rectangle(xpos, ypos,SIZE,SIZE)
     ctx.fill()
-
 
 i = 1
 j = 0
@@ -56,12 +51,14 @@ print("Primes found")
 while(j < len(primeArray)):
 
     if primeArray[j] == i:
-        draw_prime(i)
+        prime_color()
+        draw_dot(i)
         j += 1
     #else:
-    #   draw_nonPrime(i)
+    #   nonPrime_color()
+        draw_dot(i)
     
-
+    
     i+= 1
 
 surface.write_to_png("example.png")  # Output to PNG
